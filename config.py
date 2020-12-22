410d038ee3ebe30d99f2ccc3b007005ee59a75b4
scjoiner <scjoiner@gmail.com>
2020-12-21 23:41:28 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+client_id = ""
+client_secret = ""
+redirect_uri = "http://127.0.0.1:65010/authorize_callback"
+refresh_token = ""
+password=""
+username="Judgement_Bot_AITA"
+user_agent="AITA question bot"
+message_title = "Your post from /r/AmItheAsshole won't be posted until you respond."
+message_body = "[Your post]({{post}}) will be approved once you do the following:\n\n###Please provide a clear statement explaining why you believe you might be the asshole as a reply to this bot.\n\n \
+Keep in mind a third parties opinion alone does not qualify, your conflict must be with the person your actions affected. \
+You will need to explain briefly why someone calling you an asshole for your actions caused you to believe they might be right. \
+What might you have done wrong?\n\n\
+Posts without an **interpersonal conflict** related to your action(s) or a clear statement of **why you might be the asshole** here will be removed.\n\n \
+You must respond within 30 minutes for your post to be successfully posted."
+sticky_comment = \
+"Welcome to /r/AmITheAsshole. Please view our [voting guide here](https://www.reddit.com/r/AmItheAsshole/wiki/faq#wiki_what.2019s_with_these_acronyms.3F_what_do_they_mean.3F), and remember to use **only one** judgement in your comment.\n\n\
+\
+OP has offered the following explanation for why they think they might be the asshole:\n\n \
+\
+> {{answer}}\n\n \
+\
+Help keep the sub engaging!\n\n#Don’t downvote assholes!\n\n \
+Do upvote interesting posts!\n\n \
+\
+[Click Here For Our Rules](https://www.reddit.com/r/AmItheAsshole/about/rules/) and [Click Here For Our FAQ](https://www.reddit.com/r/AmItheAsshole/wiki/faq)\n\n \
+\
+---\n\n\
+*I am a bot, and this action was performed automatically. Please [contact the moderators of this subreddit](https://www.reddit.com/message/compose/?to=/r/AmItheAsshole) if you have any questions or concerns.*"
+retry_message_title = "Your submission to /r/AmITheAsshole hasn’t been posted, please try again."
+retry_message_body = "Your response didn’t fully communicate why you think you might be the asshole. Please remember:\n\nKeep in mind a third parties opinion alone does not qualify, your conflict must be with the person your actions affected. You will need to explain briefly why someone calling you an asshole for your actions caused you to believe they might be right. What might you have done wrong?\n\n###Please provide a clear statement explaining why you believe you might be the asshole as a reply to this bot."
+timeout_message_title = "Your post to /r/amitheasshole will not be approved."
+timeout_message_body = "Your response came after the 30 minute time frame given. If we approved this after so long no one would see your post. You will need to resubmit your post and answer again when prompted."
